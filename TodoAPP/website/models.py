a504bf7e28c8f310b3b3a2f8c74609925eab8013
Vazowskyy <m.kryczka03@gmail.com>
2025-08-16 15:08:59 +0200

    Task model + create and view mechanism for todo tasks

--- TodoAPP/website/models.py
+++ TodoAPP/website/models.py
@@ -1,4 +1,5 @@
 from flask_login import UserMixin
+from sqlalchemy import func
 from . import db
 
 
@@ -7,3 +8,12 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
+    tasks = db.relationship('Task')
+
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    text = db.Column(db.String(100), default="")
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    user = db.Column(db.Integer, db.ForeignKey('user.id'))
--- TodoAPP/website/views.py
+++ TodoAPP/website/views.py
@@ -1,5 +1,7 @@
-from flask import Blueprint, render_template
-from flask_login import login_required
+from flask import Blueprint, render_template, request, redirect, url_for
+from flask_login import login_required, current_user
+from .models import Task
+from . import db
 views = Blueprint('views', __name__)
 
 
@@ -8,7 +10,17 @@ def home():
     return render_template("home.html")
 
 
-@views.route('/todo_list')
+@views.route('/todo_list', methods=['GET', 'POST'])
 @login_required
 def todo_list():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        text = request.form.get('content')
+
+        task = Task(name=name, text=text, user=current_user.id)
+        db.session.add(task)
+        db.session.commit()
+
+        return redirect(url_for('views.todo_list'))
+
     return render_template("todo_list.html")
