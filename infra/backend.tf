d25a369d762c5d39d7b56964d17c1d9fd6896c88
Vazowskyy <m.kryczka03@gmail.com>
2026-05-26 17:59:16 +0200

    Repair user_data

---
+++ infra/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "todoapp-terraform-state"
+    key            = "infra/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
+}
--- infra/user_data.sh
+++ infra/user_data.sh
@@ -1,8 +1,17 @@
-#!/bin/bash
+#!/bin/bash -xe
+
 apt update -y
-apt install -y docker.io docker-compose-plugin git
+
+apt install -y \
+  docker.io \
+  docker-compose-plugin \
+  git
 
 systemctl enable docker
 systemctl start docker
+systemctl restart docker
 
 usermod -aG docker ubuntu
+
+docker --version
+docker compose version
